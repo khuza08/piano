e342b9181eb6855247e4b7b9b101275ecda2c7ed
huza <huzaifahasif3@gmail.com>
2026-03-18 18:49:06 +0700

    feat: increase active dim and highlight on keys

--- src/components/PianoKey.tsx
+++ src/components/PianoKey.tsx
@@ -29,8 +29,8 @@ export const PianoKey: React.FC<PianoKeyProps> = ({
                 onTouchEnd={(e) => { e.preventDefault(); onStop(note); }}
                 className={`
                     absolute top-0 z-20 h-[60%] transition-all duration-75 flex flex-col items-center justify-end pb-2
-                    bg-gradient-to-b from-[#303030] to-[#000000] border border-black rounded-b-sm shadow-lg active:scale-95
-                    ${active ? 'bg-gradient-to-b from-[#595959] to-[#303030] border-[#B5B5B5]' : ''}
+                    bg-gradient-to-b from-[#303030] to-[#000000] border border-black rounded-b-sm shadow-lg
+                    ${active ? 'brightness-[2] border-[#B5B5B5]' : ''}
                 `}
                 style={{ 
                     width: '1.8%',
@@ -46,21 +46,24 @@ export const PianoKey: React.FC<PianoKeyProps> = ({
     }
 
     return (
-        <button
-            onMouseDown={() => onPlay(note)}
-            onMouseUp={() => onStop(note)}
-            onMouseLeave={() => onStop(note)}
-            onTouchStart={(e) => { e.preventDefault(); onPlay(note); }}
-            onTouchEnd={(e) => { e.preventDefault(); onStop(note); }}
-            className={`
-                flex-1 h-full min-w-0 transition-all duration-75 flex flex-col items-center justify-end pb-4
-                bg-gradient-to-b from-white to-[#E0E0E0] border border-[#B5B5B5] rounded-b-md shadow-md active:translate-y-1
-                ${active ? 'bg-gradient-to-b from-[#B5B5B5] to-[#E0E0E0] text-[#303030] translate-y-1 shadow-[#B5B5B5]/50' : ''}
-            `}
-        >
-            <span className={`text-[8px] font-bold pointer-events-none ${active ? 'text-black' : 'text-[#303030]'}`}>
-                {label}
-            </span>
-        </button>
+      <button
+          onMouseDown={() => onPlay(note)}
+          onMouseUp={() => onStop(note)}
+          onMouseLeave={() => onStop(note)}
+          onTouchStart={(e) => { e.preventDefault(); onPlay(note); }}
+          onTouchEnd={(e) => { e.preventDefault(); onStop(note); }}
+          className={`
+              flex-1 h-full min-w-0 transition-all duration-75 flex flex-col items-center justify-end pb-4
+              border border-[#B5B5B5] rounded-b-md shadow-md
+              ${active 
+                  ? 'bg-[#B5B5B5]' 
+                  : 'bg-white'
+              }
+          `}
+      >
+          <span className={`text-[8px] font-bold pointer-events-none transition-colors duration-75 text-black`}>
+              {label}
+          </span>
+      </button>
     );
 };
