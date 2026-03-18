b7ba5cab5f0f5970e7af325c94081cbfcbbf9918
huza <huzaifahasif3@gmail.com>
2026-03-18 18:07:58 +0700

    reduce maxVolume
    - earrape
    
    Signed-off-by: huza <huzaifahasif3@gmail.com>

--- src/components/Piano.tsx
+++ src/components/Piano.tsx
@@ -166,7 +166,7 @@ export const Piano = () => {
                 </div>
 
                 <div className="flex items-center gap-4 w-32 ml-4">
-                    <Volume2 className="w-3 h-3 text-[#B5B5B5]" /><input type="range" min="-40" max="12" value={volume} onChange={(e) => setVolume(Number(e.target.value))} className="w-full h-1 bg-white/10 rounded-lg appearance-none cursor-pointer accent-[#B5B5B5]" />
+                    <Volume2 className="w-3 h-3 text-[#B5B5B5]" /><input type="range" min="-40" max="6" value={volume} onChange={(e) => setVolume(Number(e.target.value))} className="w-full h-1 bg-white/10 rounded-lg appearance-none cursor-pointer accent-[#B5B5B5]" />
                 </div>
             </div>
 
